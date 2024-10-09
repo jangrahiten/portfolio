29ac907e1ae02c92071c2a95b89915de052b8dc8
jangrahiten <jangrahiten.dev@gmail.com>
2024-10-09 18:06:33 +0530

    formatted with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ const App = () => {
          <div className="overflow-x-hidden text-neutral-300 selection:bg-cyan-400 selection:text-cyan-900">
             <div className="fixed top-0 -z-10 h-full w-full"></div>
             <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
-
             <div className="container mx-auto px-0">
                <Navbar />
                <Hero />
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import { HERO_CONTENT } from '../constants'
-import ProfilePicture from '../assets/profilepic.jpg'
+import React from "react";
+import { HERO_CONTENT } from "../constants";
+import ProfilePicture from "../assets/profilepic.jpg";
 const Hero = () => {
-  return (
-    <>
-      <div className='border-b border-neutral-950  pb-4 lg:mb-3.5'>
-        <div className="flex flex-wrap">
-            <div className="w-full lg:w-1/2">
-                <div className="flex flex-col items-center lg:items-start">
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight  lg:mt-16 lg:text-8xl'>Hiten Jangra</h1>
-                </div>
-                <span className='bg-gradient-to-r from-purple-700 via-neutral-700 via-slate-600 to-purple-700 bg-clip-text text-4xl tracking-tight text-transparent font-bold text-5xl'>Frontend Developer</span>
-                <p className='my-2 max-w-xl py-6 font-light tracking-tighter text-2xl'>
-                  {HERO_CONTENT}
-                </p>
+   return (
+      <>
+         <div className="border-b border-neutral-950  pb-4 lg:mb-3.5">
+            <div className="flex flex-wrap">
+               <div className="w-full lg:w-1/2">
+                  <div className="flex flex-col items-center lg:items-start">
+                     <h1 className="pb-16 text-6xl font-thin tracking-tight  lg:mt-16 lg:text-8xl">
+                        Hiten Jangra
+                     </h1>
+                  </div>
+                  <span className="bg-gradient-to-r from-purple-700 via-neutral-700 to-purple-700 bg-clip-text tracking-tight text-transparent font-bold text-5xl">
+                     Frontend Developer
+                  </span>
+                  <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-2xl">
+                     {HERO_CONTENT}
+                  </p>
+               </div>
+               <div className="w-full lg:w-1/2 lg:p-8">
+                  <div className="flex justify-center">
+                     <img
+                        src={ProfilePicture}
+                        alt="image"
+                        className="h-full rounded-lg"
+                     />
+                  </div>
+               </div>
             </div>
-        <div className='w-full lg:w-1/2 lg:p-8'>
-        <div className="flex justify-center">
-          <img src={ProfilePicture} alt="image" className='h-full rounded-lg' />
-          </div></div>
-        </div>
-      </div>
-    </>
-  )
-}
+         </div>
+      </>
+   );
+};
 
-export default Hero
+export default Hero;
