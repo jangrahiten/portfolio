825276bfc460aa77f8e8d3d739786df2efd37528
jangrahiten <jangrahiten.dev@gmail.com>
2024-10-09 09:07:36 +0530

    added some content

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Hero = () => {
   return (
     <>
-      <div className='border-b border-neutral-900 pb-4 lg:mb-3.5'>
+      <div className='border-b border-neutral-950  pb-4 lg:mb-3.5'>
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg:items-start">
-                    <h1>Hiten Jangra</h1>
+                    <h1 className='pb-16 text-6xl font-thin tracking-tight  lg:mt-16 lg:text-8xl'>Hiten Jangra</h1>
                 </div>
+                <span className='bg-gradient-to-r from-pink-600 via-slate-500 to-green-900 bg-clip-text text-4xl tracking-tight text-transparent font-bold '>Frontend Developer</span>
             </div>
         </div>
       </div>
