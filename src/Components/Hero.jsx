3748340435e4dbabeb9c27e4ee3ad789e225ad3a
jangrahiten <jangrahiten.dev@gmail.com>
2024-10-09 18:32:28 +0530

    temperory site finished

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import ProfilePicture from "../assets/profilepic.jpg";
+import { FaReact } from "react-icons/fa";
+import { SiTypescript } from "react-icons/si";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { FaNodeJs } from "react-icons/fa6";
+import { TbBrandThreejs } from "react-icons/tb";
+
 const Hero = () => {
    return (
       <>
@@ -19,9 +27,15 @@ const Hero = () => {
                      {HERO_CONTENT}
                   </p>
                   <div className=""><span className="pb-16 text-4xl tracking-tight  lg:mt-16 lg:text-5xl
-                  bg-gradient-to-br from-cyan-500 via-green-600 to-slate-700 bg-clip-text text-transparent font-bold">My Tech stack:</span>
-                    <div>
-                    
+                  bg-gradient-to-br from-cyan-500 via-green-600 to-slate-700 bg-clip-text text-transparent font-bold">My Tech stack</span>
+                    <div className="flex items-center lg:items-start flex-row gap-4 text-4xl lg:text-6xl m-2 mt-5">
+                    <FaReact  className="text-cyan-500"/>
+                    <SiTypescript className="text-yellow-500"/>
+                    <RiNextjsFill className=""/>
+                    <SiMongodb className="text-green-600"/>
+                    <SiExpress className=""/>
+                    <FaNodeJs className="text-green-600"/>
+                    <TbBrandThreejs className=""/>
                     </div>
                     </div>
                </div>
